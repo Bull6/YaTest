21f554fc834946995b9924c21813354bd61cd4aa
Bull6 <d.i.taigin@gmail.com>
2021-06-23 16:49:26 +0500

    add 4 test of yandex search

--- SearchTest.py
+++ SearchTest.py
@@ -1,12 +1,13 @@
 import pytest
+
 import selenium
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 from selenium import webdriver
 
 chromedriver = "C:/Users/DenT/Downloads/chromedriver.exe"
+
 driver = webdriver.Chrome(chromedriver)
 
 driver.implicitly_wait(5)
